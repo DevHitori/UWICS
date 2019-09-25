8337c51fe3946c4c5fb8b63312d08b20e2fb6cb0
BruceTheVeteran <BruceTheVeteran@hotmail.com>
2019-09-25 09:49:09 -0400

    boilerplate

---
+++ client/src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import axios from 'axios'
+import logo from './logo.svg';
+import './App.css';
+
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      data: null
+    }
+
+  }
+
+  async componentDidMount() {
+    let res = await axios.get('/users');
+    console.log(res.data);
+    this.setState({data: res.data.success});
+  }
+
+
+  render() {
+        return (
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  Edit <code>src/App.js</code> and save to reload!
+                  {this.state.data}
+                </p>
+                <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                Learn React
+                </a>
+          </header>
+        </div>
+        );
+      }
+
+
+
+}
